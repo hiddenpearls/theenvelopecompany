c4b88241de382cb611886d071e50688a1512b01f
cinthya.salazar <cinthya.salazar@oktara.com>
2016-10-04 09:13:32 -0600

    Contact Us Page

--- wp-content/themes/arizona-envelope/template-contact.php
+++ wp-content/themes/arizona-envelope/template-contact.php
@@ -5,6 +5,6 @@
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php //get_template_part('templates/page', 'header'); ?>
+  <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/partials/content', 'contact'); ?>
 <?php endwhile; ?>
--- wp-content/themes/arizona-envelope/templates/header.php
+++ wp-content/themes/arizona-envelope/templates/header.php
@@ -91,3 +91,43 @@ if( is_front_page() ){
         </div>
     </div>
 <?php }?>
+
+<?php if(is_page('contact-us')){?>
+    <div class="category-shop-header">
+        <div class="container">
+            <ul class="shop-navigation-bar">
+                <?php
+
+                      $taxonomy     = 'product_cat';
+                      $orderby      = 'name';  
+                      $show_count   = 0;      // 1 for yes, 0 for no
+                      $pad_counts   = 0;      // 1 for yes, 0 for no
+                      $hierarchical = 0;      // 1 for yes, 0 for no  
+                      $title        = '';  
+                      $empty        = 0;
+
+                      $args = array(
+                             'taxonomy'     => $taxonomy,
+                             'orderby'      => $orderby,
+                             'show_count'   => $show_count,
+                             'pad_counts'   => $pad_counts,
+                             'hierarchical' => $hierarchical,
+                             'title_li'     => $title,
+                             'hide_empty'   => $empty
+                      );
+                     $all_categories = get_categories( $args );
+                     foreach ($all_categories as $cat) {
+                        $counter = 1;
+                        if($cat->category_parent == 0) {
+                            $category_id = $cat->term_id;   
+                            echo '<li>';    
+                              echo  '<a class="shop-nav-btn" href="'. get_term_link($cat->slug, 'product_cat') .'">'.$cat->name.'</a>';
+                              echo '</li>';
+                        } 
+                        $counter++;      
+                    }
+                    ?>
+            </ul>
+        </div>
+    </div>
+<?php }?>
