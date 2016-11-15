58c441bd2df69188a4dc4491df75d5749658d4fc
Andres.Castillo <andres.castillo@oktara.com>
2016-11-15 17:42:51 -0600

    Fixes to bug stories #201813, #201947, #201851 and #201782. Started working on #201791 and #201791, not completely fixed.

--- wp-content/themes/arizona-envelope/templates/page-header.php
+++ wp-content/themes/arizona-envelope/templates/page-header.php
@@ -5,6 +5,9 @@
         <div class="col-md-8 col-md-offset-2 text-center">
         	<?php if(is_page('resources')){ ?>
         		<h1 class="title-pages"><?= the_field('title'); ?></h1>
+        	<?php }elseif(is_wc_endpoint_url()){ ?>
+				<?php $string = wc_endpoint_url( $endpoint ); ?>
+				<h1><?php echo $string; ?></h1>
         	<?php }else{ ?>
             	<h1 class="title-pages"><?= Titles\title(); ?></h1>
             <?php }?>
