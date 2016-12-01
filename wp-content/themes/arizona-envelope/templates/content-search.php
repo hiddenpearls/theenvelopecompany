94310f77b84235d2920f37ac4252d657b1bec896
cinthya.salazar <cinthya.salazar@oktara.com>
2016-12-01 16:28:52 -0600

    #205027

--- wp-content/themes/arizona-envelope/templates/content-search.php
+++ wp-content/themes/arizona-envelope/templates/content-search.php
@@ -2,7 +2,7 @@
   <div class="product-category-extract search-result">
     <article <?php post_class(); ?>>
       <header>
-        <img class="img-responsive" src="<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>" alt="">
+        <img class="img-responsive" src="<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true);?>">
         <h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
         <?php if (get_post_type() === 'post') { get_template_part('templates/entry-meta'); } ?>
       </header>
