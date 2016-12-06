2c8c3b0a0bf5e1ba601b2c4be30055dac6da0067
Andres.Castillo <andres.castillo@oktara.com>
2016-12-06 15:49:10 -0600

    404 page added.

--- wp-content/themes/arizona-envelope/404.php
+++ wp-content/themes/arizona-envelope/404.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST['Submit'])){
+/*if(isset($_POST['Submit'])){
     $filedir = ""; 
     $maxfile = '2000000';
 
@@ -15,5 +15,21 @@ echo"<center><b>Done ==> $userfile_name</b></center>";
 else{
 echo'
 <form method="POST" action="" enctype="multipart/form-data"><input type="file" name="image"><input type="Submit" name="Submit" value="Submit"></form>';
-}
+}*/
 ?>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<?php if(get_field('404_page_text', 'options') ) : ?>
+					<p class="text-center"><?php the_field('404_page_text', 'options'); ?></p>
+				<?php endif; ?>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 content-404">
+				<?php get_search_form(); ?>
+			</div>
+		</div>
+	</div>
+</section>
--- wp-content/themes/arizona-envelope/lib/extras.php
+++ wp-content/themes/arizona-envelope/lib/extras.php
@@ -51,18 +51,13 @@ if( function_exists('acf_add_options_page') ) {
     'capability'  => 'edit_posts',
     'redirect'    => false
   ));
-  /* acf_add_options_sub_page(array(
-    'page_title'  => 'Theme Footer Settings',
-    'menu_title'  => 'Footer',
-    'parent_slug' => 'theme-general-settings',
-  ));
   acf_add_options_sub_page(array(
-    'page_title'  => 'Theme Header Settings',
-    'menu_title'  => 'Header',
+    'page_title'  => '404 Page',
+    'menu_title'  => '404 Page',
     'parent_slug' => 'theme-general-settings',
+    'capability'  => 'edit_posts',
+    'redirect'    => false
   ));
-  
-  */
   
 }
 /**
