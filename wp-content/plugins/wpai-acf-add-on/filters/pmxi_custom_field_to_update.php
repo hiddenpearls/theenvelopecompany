e9182e722b28fca5eae0849391854d80abf19f96
root <root@ip-172-31-9-125.us-west-2.compute.internal>
2017-01-10 19:55:16 +0000

    deleted plugins and updated database

--- wp-content/plugins/wpai-acf-add-on/filters/pmxi_custom_field_to_update.php
+++
@@ -1,8 +0,0 @@
-<?php
-function pmai_pmxi_custom_field_to_update( $field_to_update, $post_type, $options, $m_key ){	
-
-	if ( ! in_array($m_key, PMAI_Plugin::$all_acf_fields) ) return $field_to_update;		
-
-	return pmai_is_acf_update_allowed($m_key, $options);
-}
-?>
