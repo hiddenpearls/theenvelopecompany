03de637030006449ef27b4467c2b577ed960d861
Andres.Castillo <andres.castillo@oktara.com>
2017-04-26 17:57:17 -0600

    Email recipient fixed.

--- wp-content/plugins/oktara-content-types/includes/class-wc-custom-order-email.php
+++ wp-content/plugins/oktara-content-types/includes/class-wc-custom-order-email.php
@@ -1,12 +1,6 @@
 <?php
-
-// need to create a class to extend the custom wc email
-// add the different default values to the email
-// validate the email should be send when the new order is placed
-
-
 /**
- * A custom Expedited Order WooCommerce Email class
+ * A custom Order WooCommerce Email class
  *
  * @since 0.1
  * @extends \WC_Email
@@ -33,21 +27,23 @@ class WC_Custom_Order_Email extends WC_Email {
 		// Trigger on new paid orders
 		add_action( 'woocommerce_order_status_pending_to_processing_notification', array( $this, 'trigger' ) );
 		add_action( 'woocommerce_order_status_failed_to_processing_notification',  array( $this, 'trigger' ) );
-		//add_action( 'woocommerce_order_status_pending_to_processing_notification', array( $this, 'trigger' ) );
-		//add_action( 'woocommerce_order_status_pending_to_completed_notification', array( $this, 'trigger' ) );
-		//add_action( 'woocommerce_order_status_pending_to_on-hold_notification', array( $this, 'trigger' ) );
-		//add_action( 'woocommerce_order_status_failed_to_processing_notification', array( $this, 'trigger' ) );
-		//add_action( 'woocommerce_order_status_failed_to_completed_notification', array( $this, 'trigger' ) );
-		//add_action( 'woocommerce_order_status_failed_to_on-hold_notification', array( $this, 'trigger' ) );
 		// Call parent constructor to load any other defaults not explicity defined here
 		parent::__construct();
 		// this sets the recipient to the settings defined below in init_form_fields()
-		$this->recipient = $this->get_option( 'recipient' );
+		//$this->recipient = $this->get_option( 'recipient' );
 		// if none was entered, just use the WP admin email as a fallback
-		if ( ! $this->recipient )
-			$this->recipient = get_option( 'admin_email' );
+		//if ( ! $this->recipient ){
+			//$this->recipient = get_option( 'admin_email' );
+		//}
+		
+		//$this->recipient = apply_filters( 'woocommerce_email_recipient_{$id}', $this->recipient, $this->object );
+		
+		/*global $woocommerce;
+		$this->object = new WC_Order( $order_id );
+		$customer = get_userdata($this->object->customer_user);
+		var_dump($customer);*/
 	}
-
+	
 	/**
 	 * Determine if the email should actually be sent and setup email merge variables
 	 * trigger function
@@ -65,7 +61,11 @@ class WC_Custom_Order_Email extends WC_Email {
 		$this->replace[] = date_i18n( woocommerce_date_format(), strtotime( $this->object->order_date ) );
 		$this->find[] = '{order_number}';
 		$this->replace[] = $this->object->get_order_number();
-		
+		$this->recipient    = $this->object->billing_email;
+		error_log($this->recipient);
+		//$customer = get_userdata($this->object->customer_user);
+		//var_dump($customer);
+
 		if ( ! $this->is_enabled() || ! $this->get_recipient() )
 			return;
 		$this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
@@ -113,13 +113,6 @@ class WC_Custom_Order_Email extends WC_Email {
 				'label'   => 'Enable this email notification',
 				'default' => 'yes'
 			),
-			'recipient'  => array(
-				'title'       => 'Recipient(s)',
-				'type'        => 'text',
-				'description' => sprintf( 'Enter recipients (comma separated) for this email. Defaults to <code>%s</code>.', esc_attr( get_option( 'admin_email' ) ) ),
-				'placeholder' => '',
-				'default'     => ''
-			),
 			'subject'    => array(
 				'title'       => 'Subject',
 				'type'        => 'text',
@@ -150,3 +143,18 @@ class WC_Custom_Order_Email extends WC_Email {
 	}
 
 }// end \WC_Custom_Order_Email class
+
+
+
+
+
+/*
+,
+'recipient'  => array(
+	'title'       => 'Recipient(s)',
+	'type'        => 'text',
+	'description' => sprintf( 'Enter recipients (comma separated) for this email. Defaults to <code>%s</code>.', esc_attr( get_option( 'admin_email' ) ) ),
+	'placeholder' => '',
+	'default'     => ''
+)
+*/
