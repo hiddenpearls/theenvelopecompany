0ba5f106d67d7bf3a648227c078e6188ded7a073
Andres.Castillo <andres.castillo@oktara.com>
2017-08-07 20:44:57 -0600

    Story #268650, Epik #223457: orders page changes to include PO #. Changes in gulpfile to include woocommerce templates in the watch browser sync task.

--- wp-content/themes/arizona-envelope/woocommerce/myaccount/orders.php
+++ wp-content/themes/arizona-envelope/woocommerce/myaccount/orders.php
@@ -22,14 +22,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-do_action( 'woocommerce_before_account_orders', $has_orders ); ?>
+do_action( 'woocommerce_before_account_orders', $has_orders );
+/**
+ * Get My Account > Orders columns.
+ *
+ * @since 2.6.0
+ * @return array
+ */
+ 
+function azenv_wc_get_account_orders_columns() {
+	$columns = apply_filters( 'woocommerce_account_orders_columns', array(
+		'order-number'  => __( 'Order', 'woocommerce' ),
+		'order-date'    => __( 'Date', 'woocommerce' ),
+		'order-status'  => __( 'Status', 'woocommerce' ),
+        'order-po'      => __('PO#', 'woocommerce'),
+		'order-total'   => __( 'Total', 'woocommerce' ),
+		'order-actions' => '&nbsp;',
+	) );
+
+	// Deprecated filter since 2.6.0.
+	return apply_filters( 'woocommerce_my_account_my_orders_columns', $columns ); 
+}
+?>
 
 <?php if ( $has_orders ) : ?>
 
 	<table class="woocommerce-MyAccount-orders shop_table shop_table_responsive my_account_orders account-orders-table">
 		<thead>
 			<tr>
-				<?php foreach ( wc_get_account_orders_columns() as $column_id => $column_name ) : ?>
+				<?php foreach ( azenv_wc_get_account_orders_columns() as $column_id => $column_name ) : ?>
 					<?php if( $column_name != 'Status' ){  ?>
 						<th class="<?php echo esc_attr( $column_id ); ?>"><span class="nobr"><?php echo esc_html( $column_name ); ?></span></th>
 					<?php } ?>
@@ -43,10 +64,14 @@ do_action( 'woocommerce_before_account_orders', $has_orders ); ?>
 				//echo "<pre>";
 				//print_r($order);
 				//echo "</pre>";
+                // Store the order ID and get the PO Number for that specific order
+                $order_retrieve = $order->id;
+                global $wpdb;
+                $results = $wpdb->get_results("SELECT `meta_value` FROM wp_postmeta WHERE `post_id`=$order_retrieve AND `meta_key`='po_reference_number'");
 				$item_count = $order->get_item_count();
 				?>
 				<tr class="order">
-					<?php foreach ( wc_get_account_orders_columns() as $column_id => $column_name ) : ?>
+					<?php foreach ( azenv_wc_get_account_orders_columns() as $column_id => $column_name ) : ?>
 						<?php if( $column_name != 'Status' ){  ?>
 							<td class="<?php echo esc_attr( $column_id ); ?>" data-title="<?php echo esc_attr( $column_name ); ?>">
 								<?php if ( has_action( 'woocommerce_my_account_my_orders_column_' . $column_id ) ) : ?>
@@ -61,7 +86,11 @@ do_action( 'woocommerce_before_account_orders', $has_orders ); ?>
 									<time datetime="<?php echo date( 'Y-m-d', strtotime( $order->order_date ) ); ?>" title="<?php echo esc_attr( strtotime( $order->order_date ) ); ?>"><?php echo date_i18n( get_option( 'date_format' ), strtotime( $order->order_date ) ); ?></time>
 
 								<?php //elseif ( 'order-status' === $column_id ) : ?>
-									<?php //echo wc_get_order_status_name( $order->get_status() ); ?>
+                                    <?php //echo wc_get_order_status_name( $order->get_status() ); ?>
+                                
+                                <?php elseif ( 'order-po' === $column_id ) : ?>
+                                    <?php echo $results[0]->meta_value; ?>
+                                    <?php //do_action( 'woocommerce_order_details_after_customer_details', $order ); ?>
 
 								<?php elseif ( 'order-total' === $column_id ) : ?>
 									<?php echo sprintf( _n( '%s for %s item', '%s for %s items', $item_count, 'woocommerce' ), $order->get_formatted_order_total(), $item_count*500 ); ?>
